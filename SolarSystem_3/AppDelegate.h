c1cb24154519cdf7b0ce392f94a7596694266672
lam9009 <lam9009@yahoo.com>
2015-02-17 12:33:21 -0800

    Initial Commit

---
+++ SolarSystem_3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SolarSystem_3
+//
+//  Created by Alfred on 17/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SolarSystem_3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SolarSystem_3
+//
+//  Created by Alfred on 17/2/15.
+//  Copyright (c) 2015 Maptier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
